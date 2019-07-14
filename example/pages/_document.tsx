ed240dc5debb37c7a597a206e6815d6bbcfa9b43
Tomek Mularczyk <tomaszmularczyk89@gmail.com>
2019-07-14 11:43:58 +0200

    refactor _document in example

--- example/pages/_document.tsx
+++ example/pages/_document.tsx
@@ -1,34 +1,14 @@
-import React from "react";
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from "next/document";
-import { ResetHtmlIdGenerator } from "../../lib";
+import Document, { DocumentContext } from "next/document";
+import { resetId } from "../../lib";
 
-// _document is only rendered on the server side and not on the client side
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
+    // _document is only rendered on the server side and not on the client side
+    // this will reset id keeping markup consistent across server and browser
+    resetId();
+
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          {/* 
-             this will reset id keeping markup 
-             consistent across server and browser*/}
-          <ResetHtmlIdGenerator />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
 
